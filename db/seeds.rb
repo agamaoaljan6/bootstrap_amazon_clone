afa2fcabbea1b230e89dd089d6bb9112059d72fb
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-05 12:57:00 -0700

    Edited some css styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,71 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
 
 # To run your seeds, do:
 # rails db:seed
 
-Product.destroy_all
 
-NUM_QUESTIONS = 1000
+NUM_QUESTIONS = 200
+NUM_USERS = 10
+PASSWORD = "supersecret"
+
+Answer.delete_all
+Question.delete_all
+User.delete_all
+
+super_user = User.create(
+  first_name: "Jon",
+  last_name: "Snow",
+  email: "js@winterfell.gov",
+  password: PASSWORD
+)
+
+NUM_USERS.times do
+  first_name =  Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
+    password: PASSWORD
+  )
+end
+users = User.all
 
 NUM_QUESTIONS.times do
   created_at = Faker::Date.backward(365 * 5)
-  q=Product.create(
+  q = Question.create(
     # Faker is a ruby module. We access classes
     # or other modules inside of it with ::.
     # Here, Hacker is a class inside of the
     # Faker module
     title: Faker::Hacker.say_something_smart,
-    description: Faker::ChuckNorris.fact,
-    price: Faker::Commerce.price(range=0...2000.0),
+    body: Faker::ChuckNorris.fact,
     created_at: created_at,
-    updated_at: created_at
+    updated_at: created_at,
+    user: users.sample
   )
-    if q.valid?
-      q.reviews = rand(0..15).times.map do
-        Review.new({
-          rating: Faker::Commerce.price(range=1..5.0),
-          body: Faker::GreekPhilosophers.quote})
-      end
+  if q.valid?
+    q.answers = rand(0..15).times.map do
+      Answer.new(
+        body: Faker::GreekPhilosophers.quote,
+        user: users.sample
+      )
     end
+  end
 end
 
-product = Product.all
-review=Review.all
+questions = Question.all
+answers = Answer.all
 
-puts Cowsay.say("Generated #{product.count} product", :frogs)
-puts Cowsay.say("Generated #{review.count} reviews", :tux)
+puts Cowsay.say("Generated #{questions.count} questions", :frogs)
+puts Cowsay.say("Generated #{answers.count} answers", :tux)
+puts Cowsay.say("Generated #{users.count} user", :stegosaurus)
+
+puts "Login with #{super_user.email} and password: #{PASSWORD}"

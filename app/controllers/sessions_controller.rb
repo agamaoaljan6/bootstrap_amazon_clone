32ed53497126403f590e1ebbfad030306ebdbc92
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-08 11:07:08 -0700

    fixed logout notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
       
     def destroy 
         session[:user_id] = nil
-        redirect_to root_path, notice: "Logged Out!"
+        flash[:success] = "Logged out!"
+        redirect_to root_path
     end 
 end

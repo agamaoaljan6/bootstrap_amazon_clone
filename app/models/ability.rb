2be8ee2ae6fa1e31692bf82b43fd69154bc40d71
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-08 11:04:07 -0700

    added useradmin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,13 +6,15 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+      user ||= User.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
     #   else
     #     can :read, :all
     #   end
-    #
+    if user.is_admin?
+      can :manage, :all
+      end 
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
@@ -37,7 +39,10 @@ class Ability
     can(:crud, Product) do |product|
       product.user == user 
     end 
-
+    can(:crud, Review) do |review|
+      # review.user == user || review.question.user = user
+      review.user == user
+    end
 
 
   end
